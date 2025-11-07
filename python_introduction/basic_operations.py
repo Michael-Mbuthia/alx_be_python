91861ba29bef4a3b595edf93fe250d8ecc217643
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-08 00:56:02 +0300

    Perform various arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Perform basic arithmetic operations
+sum = number1 + number2
+print("Addition of", number1, "and", number2, "is", sum)
+
+difference = number1 - number2
+print("Subtraction of", number2, "from", number1, "is", difference)
+
+product = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", product)
