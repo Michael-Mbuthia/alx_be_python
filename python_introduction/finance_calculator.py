160df4394aeaa2b20abcdadde729470826118360
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-08 08:31:55 +0300

    Fix typo in monthly savings calculation variable name

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = float(input("Enter your monthly income: "))
 total_monthly_expenses = float(input("Enter your total monthly expenses: "))
-monthly_savings = monthly_income - total_montly_expenses 
+monthly_savings = monthly_income - total_monthly_expenses 
 projected_savings = monthly_savings * 12 + (montly_savings * 12 * 0.05)
 
 print("Your monthly savings are $", monthly_savings, ".")
