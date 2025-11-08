7e811f0520829be7a1478819e2879af9554e4a12
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-08 07:00:15 +0300

    Fix input prompt wording in future age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-age = int (input("How old are you: "))
+age = int (input("How old are you? "))
 years_to_add = 27
 future_age = int(age) + int(years_to_add)
 print("In 2025, you will be", future_age, )
