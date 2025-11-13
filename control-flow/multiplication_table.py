25f3dfbd4e79cdf131ffa62d0038a9f6e295bd1a
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-13 09:03:04 +0300

    Add multiplication table program to display user input results

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+#Get user input
+number = int(input("Enter a number to see its multiplication table: "))
+
+for numbers in range(1, 11):
+    Z = number * numbers
+    print(f"{number} * {numbers} = {Z}")
