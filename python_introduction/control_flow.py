866e5a2c52aa70a41828096e15a51496dd092618
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-13 06:35:59 +0300

    Add weather advice program and implement number guessing game

---
+++ python_introduction/control_flow.py
@@ -0,0 +1,15 @@
+score = input("Enter your score: ")
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+elif score >= 50:
+    print("E")
+else:
+    print("F") 
+print(f"Your score is {score}")
--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
 age = int (input("How old are you? "))
-years_to_add = 27
-future_age = int(age) + int(years_to_add)
-print("In 2025, you will be", future_age, )
+Years_To_Add = 27
+future_age = int(age) + int(Years_To_Add)
+print("In 2052, you will be", future_age, )
