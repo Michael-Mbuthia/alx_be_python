0bdc56ebe01e743408c3e48690d1b035710795ad
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-22 21:24:04 +0300

    Refactor display_current_datetime function: make current_date parameter optional

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,16 +1,19 @@
 from datetime import datetime, timedelta
 
-def display_current_datetime(current_date):
+def display_current_datetime(current_date=None):
+    # Accepts an optional datetime; if none provided use now()
+    if current_date is None:
+        current_date = datetime.now()
     print(f"Current date and time: {current_date}")
 
 def future_date(current_date):
     number_of_days = int(input("Enter the number of days to add to the current date: "))
-    new_date = current_date + timedelta(days=number_of_days)
-    print(f"Future date: {new_date}")
+    future_date = current_date + timedelta(days=number_of_days)
+    print(f"Future date: {future_date}")
 
 def main():
     current_date = datetime.now()
-    display_current_datetime(current_date)
+    display_current_datetime()   
     future_date(current_date)
 
 if __name__ == "__main__":
