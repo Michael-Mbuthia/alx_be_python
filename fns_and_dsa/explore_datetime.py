fe46ef2acab3901e3015c4ddb4e50ca1d380c1f8
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2025-11-22 21:25:27 +0300

    Refactor future_date function: rename to calculate_future_date for clarity

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,7 +6,7 @@ def display_current_datetime(current_date=None):
         current_date = datetime.now()
     print(f"Current date and time: {current_date}")
 
-def future_date(current_date):
+def calculate_future_date(current_date):
     number_of_days = int(input("Enter the number of days to add to the current date: "))
     future_date = current_date + timedelta(days=number_of_days)
     print(f"Future date: {future_date}")
@@ -14,7 +14,7 @@ def future_date(current_date):
 def main():
     current_date = datetime.now()
     display_current_datetime()   
-    future_date(current_date)
+    calculate_future_date(current_date)
 
 if __name__ == "__main__":
     main()
